0cba5586c08a7d32c0ae144567cc12a0a50e4dbd
sadia1804 <145155815+sadia1804@users.noreply.github.com>
2023-09-16 00:20:46 +0530

    Create sjf.c

---
+++ sjf.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+int bt[20],wt[20],tat[20],p[20],i,n,k,temp;
+float wtavg,tatavg;
+printf("enter no. of processes:");
+scanf("%d",&n);
+printf("enter bt's:");
+for(i=0;i<n;i++)
+{
+p[i]=i;
+scanf("%d",&bt[i]);
+}
+for(i=0;i<n;i++)
+{
+for(k=i+1;k<n;k++)
+{
+if(bt[i]>bt[k])
+{
+temp=bt[i];
+bt[i]=bt[k];
+bt[k]=temp;
+temp=p[i];
+p[i]=p[k];
+p[k]=temp;
+}
+}
+wt[0]=wtavg=0;
+tat[0]=tatavg=bt[0];
+}
+for(i=1;i<n;i++)
+{
+wt[i]=wt[i-1]+bt[i-1];
+tat[i]=tat[i-1]+bt[i-1];
+wtavg=wtavg+wt[i];
+tatavg=tatavg+tat[i];
+}
+printf("\nprocess\tbt\twt\ttat\n");
+for(i=0;i<n;i++)
+printf("\np%d\t%d\t%d\t%d\n",p[i],bt[i],wt[i],tat[i]);
+printf("wtavg=%f, tatavg=%f",wtavg,tatavg);
+}
